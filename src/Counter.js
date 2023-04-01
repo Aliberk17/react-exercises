384d8bbf46815cd95fdaec2a63ff514ce3c000cc
Aliberk17 <89722075+Aliberk17@users.noreply.github.com>
2023-04-01 21:59:34 +0300

    State 01

---
+++ src/Counter.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+export default class Counter extends Component {
+  state = {
+    count: 0,
+  };
+
+  constructor(props) {
+    super(props);
+    setInterval(() => {
+      this.setState({ count: this.state.count + 1 });
+    }, 1000);
+  }
+
+  render() {
+    return <h1>{this.props.count}</h1>;
+  }
+}
