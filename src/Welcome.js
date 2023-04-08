1663e4ff26f5f35f35af477bea4a5d825fb563c8
Aliberk17 <89722075+Aliberk17@users.noreply.github.com>
2023-04-08 17:28:43 +0300

    Function Components - 01

--- src/Welcome.js
+++ src/Welcome.js
@@ -1,17 +1,16 @@
-import React, { Component } from "react";
 import Age from "./Age";
 
-export default class Welcome extends Component {
-  render() {
-    return (
-      <div className="welcome">
-        <p>Welcome {this.props.name || "Default name"}!</p>
-        {this.props.age > 18 ? (
-          <Age age={this.props.age} />
-        ) : (
-          <p>You are very young!</p>
-        )}
-      </div>
-    );
-  }
+function Welcome() {
+  return (
+    <div className="welcome">
+      <p>Welcome {this.props.name || "Default name"}!</p>
+      {this.props.age > 18 ? (
+        <Age age={this.props.age} />
+      ) : (
+        <p>You are very young!</p>
+      )}
+    </div>
+  );
 }
+
+export default Welcome;
