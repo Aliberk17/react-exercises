f9fce53433ca16c2638a5a70ef62491ac4e83409
Aliberk17 <89722075+Aliberk17@users.noreply.github.com>
2023-04-08 15:30:33 +0300

    Lists - 03

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import Counter from "./Counter";
 import Hello from "./Hello";
 import InteractiveWelcome from "./InteractiveWelcome ";
 import Login from "./Login";
+import TodoList from "./TodoList";
 import UncontrolledLogin from "./UncontrolledLogin ";
 import Welcome from "./Welcome";
 
@@ -24,6 +25,7 @@ export default class App extends Component {
         <InteractiveWelcome />
         <Login onLogin={this.onLogin} />
         <UncontrolledLogin onLogin={this.onLogin} />
+        <TodoList/>
       </div>
     );
   }
---
+++ src/TodoList.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+
+export default class TodoList extends Component {
+  state = {
+    items: [],
+    inputText: "",
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      inputText: event.target.value,
+    });
+  };
+
+  handleClick = (event) => {
+    let newItems = this.state.items;
+    newItems.push(this.state.inputText);
+    this.setState({
+      items: newItems,
+      inputText: "",
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <ul>
+          {this.state.items.map((item) => (
+            <li key={item}>{item}</li>
+          ))}
+        </ul>
+        <input value={this.state.inputText} onChange={this.handleChange} />
+        <button onClick={this.handleClick}>Add to List</button>
+      </div>
+    );
+  }
+}
