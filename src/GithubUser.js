ab5065f96d1d322a6c67beef36d529c83ba6676a
Aliberk17 <89722075+Aliberk17@users.noreply.github.com>
2023-04-10 15:02:33 +0300

    Custom Hooks - 04

--- src/GithubUser.js
+++ src/GithubUser.js
@@ -1,16 +1,33 @@
+import { useEffect } from "react";
 import useGithubUser from "./useGithubUser";
 
 export default function GithubUser(props) {
-  const [response] = useGithubUser(props.username);
+  const [response, loading, error, fetchGithubUser] = useGithubUser(
+    props.username
+  );
 
-  return (
-    <div>
+  useEffect(() => {
+    fetchGithubUser();
+  }, []);
+
+  const renderLoading = () => {
+    return <p>Loading...</p>;
+  };
+
+  const renderError = () => {
+    return <p>Error!</p>;
+  };
+
+  const renderGithubUser = () => {
+    return (
       <ul>
         <li>login: {response.login}</li>
         <li>id: {response.id}</li>
         <li>node_id: {response.node_id}</li>
         <li>avatar_url: {response.avatar_url}</li>
       </ul>
-    </div>
-  );
+    );
+  };
+
+  return loading ? renderLoading() : error ? renderError() : renderGithubUser();
 }
--- src/useGithubUser.js
+++ src/useGithubUser.js
@@ -1,13 +1,21 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 
 export default function useGithubUser(username) {
   const [response, setResponse] = useState("");
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
-  useEffect(() => {
+  const fetchGithubUser = () => {
     fetch(`https:api.github.com/users/${username}`)
-      .then((e) => e.json())
-      .then((response) => setResponse(response));
-  }, []);
+      .then((e) => {
+        setLoading(false);
+        setError(e.json());
+      })
+      .then((response) => {
+        setLoading(false);
+        setResponse(response);
+      });
+  };
 
-  return [response];
+  return [response, loading, error, fetchGithubUser];
 }
