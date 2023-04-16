c797b461bbb7b586b1ecd55c171eab3bf5dc03c3
Aliberk17 <89722075+Aliberk17@users.noreply.github.com>
2023-04-16 22:15:35 +0300

    React Router - 02

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Welcome from "./Welcome";
+import Counter from "./Counter";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Welcome name={"Ali"} />}></Route>
+        <Route path="/counter" element={<Counter />}></Route>
       </Routes>
     </BrowserRouter>
   );
