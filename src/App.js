984815e4c05ce22644c71fdd83876d0816914825
Aliberk17 <89722075+Aliberk17@users.noreply.github.com>
2023-04-16 22:47:02 +0300

    React Router - 04

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
 import Welcome from "./Welcome";
 import Counter from "./Counter";
 import ShowGithubUser from "./ShowGithubUser";
@@ -6,6 +6,13 @@ import ShowGithubUser from "./ShowGithubUser";
 export default function App() {
   return (
     <BrowserRouter>
+      <div>
+        <Link to="/">Welcome</Link>
+        <br />
+        <Link to="/counter">Counter</Link>
+        <br />
+        <Link to="/users/aliberk17">ShowGithubUser</Link>
+      </div>
       <Routes>
         <Route path="/" element={<Welcome name={"Ali"} />}></Route>
         <Route path="/counter" element={<Counter />}></Route>
